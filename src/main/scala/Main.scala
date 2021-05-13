687ae664bfee15fce13eacb860677d5470153a47
Jedrzej Rochala <rochala@tutanota.com>
2021-05-13 12:17:09 +0200

    implement decimal to other system conversion

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,8 @@
 import scala.collection.immutable.TreeMap
+import scala.math.Integral.Implicits._
 
 object Main extends App {
-  val romanNumberals = TreeMap(
+  val romanNumerals = TreeMap(
     1000 -> "M",
     900 -> "CM",
     500 -> "D",
@@ -14,16 +15,36 @@ object Main extends App {
     9 -> "IX",
     5 -> "V",
     4 -> "IV",
-    1 -> "I")
-  def romanConversion(number: Int): String = {
-    val numeral = romanNumberals.rangeTo(number).maxOption
+    1 -> "I"
+  )
+  def romanConversion(number: Int, result: String = ""): String = {
+    val numeral = romanNumerals.rangeTo(number).lastOption
     numeral match {
-      case Some((value, literal)) => literal + romanConversion(number - value)
-      case None => ""
+      case Some((value, literal)) =>
+        romanConversion(number - value, result + literal)
+      case None => result
     }
   }
 
+  def systemConversion(number: Int, system: Int, result: String = ""): String = {
+    def systemNumeralConversion(number: Int, system: Int): String = {
+      if (number < 10) {
+        number.toString
+      } else {
+        (65 + number - 10).toChar.toString
+      }
+    }
 
+    number match {
+      case 0 => result
+      case _ => {
+        val (quotient, remainder) = number /% system
+        systemConversion(quotient, system, systemNumeralConversion(remainder, system) + result)
+      }
+    }
+  }
+
+  println(systemConversion(256, 16))
   println(romanConversion(210))
   println(romanConversion(2110))
   println(romanConversion(2115))
